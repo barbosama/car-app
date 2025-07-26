f60caef5ed54c562cbc85600ae510a1cb1f5a997
Marta <1180695@isep.ipp.pt>
2025-07-26 14:55:47 +0100

    added lazy loading for vehicle details page

--- car-auction/src/app/app.routes.ts
+++ car-auction/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { VehicleList } from './components/vehicle-list/vehicle-list';
-import { VehicleDetail } from './components/vehicle-detail/vehicle-detail';
 
 export const routes: Routes = [
   {
     path: '',
     component: VehicleList,
   },
-  { path: 'vehicle/:id', component: VehicleDetail },
+  {
+    path: 'vehicle/:id',
+    loadComponent: () =>
+      import('./components/vehicle-detail/vehicle-detail').then(
+        (m) => m.VehicleDetail
+      ),
+  },
 ];
